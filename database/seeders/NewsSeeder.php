6f272daac0e219ace8990862f672ea77f1b08d34
moudy757 <zoro7599@gmail.com>
2023-04-26 20:03:38 +0200

    Changed NewsSeeder to be production compatible

--- database/seeders/NewsSeeder.php
+++ database/seeders/NewsSeeder.php
@@ -15,8 +15,8 @@ class NewsSeeder extends Seeder
     {
         for ($i = 1; $i <= 25; $i++) {
             News::create([
-                'name' => fake()->name(),
-                'body' => fake()->paragraph(10),
+                'name' => 'News Article',
+                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius ipsam soluta, fuga, rerum mollitia eligendi necessitatibus animi eos ea eveniet similique optio ut dolorem quibusdam placeat non. Debitis illo, quaerat itaque quos doloremque sed aut. Labore nisi architecto explicabo voluptates, quibusdam quae. Commodi eaque nemo blanditiis ducimus, recusandae perferendis reprehenderit veniam tenetur sapiente error praesentium, voluptatem quae adipisci deleniti iste laboriosam illum cum! Totam animi omnis, consequuntur ipsa, sapiente aliquam illo ullam cupiditate voluptate earum corporis praesentium quae nulla ratione enim corrupti est perferendis quidem reprehenderit officiis. Quidem, molestias explicabo. Ea similique sapiente reprehenderit harum veritatis provident omnis neque. Ex.',
             ]);
         }
     }
